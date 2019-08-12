5cc1e32b6664d73a195915559ac2cd69b23c6ba9
Matican Matican <maticangroup@gmail.com>
2019-08-12 16:05:50 +0430

    init

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+const gulp = require('gulp');
+const browserSync = require('browser-sync').create();
+const concat = require('gulp-concat');
+
+
+function script() {
+    return gulp.src('./scripts/**/*.js')
+        .pipe(concat('all.js'))
+        .pipe(gulp.dest('./app/script/'));
+}
+
+function watch() {
+    browserSync.init({
+        server: {
+            baseDir: './app/',
+        }
+    });
+    script();
+    gulp.watch('./scripts/**/*.js', script);
+    gulp.watch('./scripts/**/*.js').on('change', browserSync.reload);
+    gulp.watch('./app/**/*.html').on('change', browserSync.reload);
+}
+
+exports.watch = watch;
